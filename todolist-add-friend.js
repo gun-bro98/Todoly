71723b19935d992ff2c650e8f4b147c334df984f
(youngho) <(one0374@naver.com)>
2020-06-05 14:47:08 +0900

    add todolist-add-friend

--- todolist-add-friend.js
+++ todolist-add-friend.js
@@ -1,14 +1,22 @@
-"use strict";
-//todolist-add-friend
-const friendBtn = document.querySelector(".friend");
-let friendName = "";
-//todolist-home
-let friendAddBtn = document.querySelector(".friend");
-function addFriendBtn() {
-  //이 함수는 친구를 클릭할시 todolist-home에 버튼이 생기게 하는 함수이다.
-
-  let icon = '<i class="fas fa-user-ninja"></i>';
-  friendAddBtn.textContent =
-    "<li><button><span>박영호</span>" + icon + "</button></li>";
+let friendList = [
+  "하동호",
+  "김덕주",
+  "박영호",
+  "김명진",
+  "양준석",
+  "백정훈",
+  "최성용",
+];
+let friendBtn = document.querySelectorAll(".friend");
+let sumTag = "";
+for (let i = 0; i < friendBtn.length; i++) {
+  friendBtn[i].addEventListener("click", () => {
+    addFriendBtn(i);
+  }); //에로우 함수를 이용해서 인수를 받게끔 만듦 원래는 addEventListener은 함수() x 함수 o 밖에 못쓴다.
 }
-// friendBtn.addEventListener("click", addFriendBtn); // friendBtn버튼을 누르는 순간에 addFriendBtn함수 실행
+function addFriendBtn(data) {
+  sumTag =
+    `<li><button><i class="fas fa-user-ninja"></i> <span>${friendList[data]}</span></button></li>` +
+    sumTag;
+  localStorage.setItem("friendItem", JSON.stringify(sumTag));
+}
--- todolist-home.js
+++ todolist-home.js
@@ -1,1 +1,13 @@
 "use strict";
+let originalData;
+let item;
+showDisplay();
+function showDisplay() {
+  let originalBtn = document.querySelector("main .main");
+  item = localStorage.getItem("friendItem");
+  if (item != null) {
+    originalData = JSON.parse(item);
+    document.querySelector("main .main").innerHTML =
+      originalBtn.innerHTML + originalData;
+  }
+}
