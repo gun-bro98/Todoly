88387d43d9c989bbfd10f8cd120eedc677c9faa1
(youngho) <(one0374@naver.com)>
2020-06-06 12:03:30 +0900

    should edit the more

--- todolist-add-friend.js
+++ todolist-add-friend.js
@@ -14,9 +14,26 @@ for (let i = 0; i < friendBtn.length; i++) {
     addFriendBtn(i);
   }); //에로우 함수를 이용해서 인수를 받게끔 만듦 원래는 addEventListener은 함수() x 함수 o 밖에 못쓴다.
 }
+
 function addFriendBtn(data) {
   sumTag =
-    `<li><button><i class="fas fa-user-ninja"></i> <span>${friendList[data]}</span></button></li>` +
+    `<li><button><i class="fas fa-user-ninja"></i> <span>${friendList[data]}</span><a class = "removeBtn"><h2>X</h2></a></button></li>` +
     sumTag;
-  localStorage.setItem("friendItem", JSON.stringify(sumTag));
+  //내가 버튼을 눌렀을 때 저장은 되지만, 다시 친구 추가 버튼을 누르고 친구들을 골라서 눌렀을 때 다시 초기화가 된다.
+  //그래서 friendItem을 자바스크립트 객체로 분해해서 더해준다음 다시 JSON문자열 형식으로 만든 것
+  //friendItem보다는 friendItem[0], friendItem[1], friendItem[2] .....friendItem[i]를 배열 형식으로 만드는 게 좋은 것 같다.second
+  //그러면 하나하나씩 넣고 뺄 수 있다.
+  let savedItem = localStorage.getItem("friendItem");
+  if (JSON.parse(savedItem) != null) {
+    savedItem = JSON.parse(savedItem) + sumTag;
+    localStorage.setItem("friendItem", JSON.stringify(savedItem));
+    let count = localStorage.getItem("count");
+    count = Number(JSON.parse(count));
+    count += 1;
+    localStorage.setItem("count", count);
+  } else {
+    localStorage.setItem("friendItem", JSON.stringify(sumTag));
+    localStorage.setItem("count", 1);
+  }
+  // localStorage.setItem("friendItem", JSON.stringify(sumTag));
 }
--- todolist-home.js
+++ todolist-home.js
@@ -2,12 +2,27 @@
 let originalData;
 let item;
 showDisplay();
+
 function showDisplay() {
   let originalBtn = document.querySelector("main .main");
   item = localStorage.getItem("friendItem");
   if (item != null) {
     originalData = JSON.parse(item);
     document.querySelector("main .main").innerHTML =
-      originalBtn.innerHTML + originalData;
+      originalData + originalBtn.innerHTML;
   }
+  makeInfo();
+}
+
+function makeInfo() {
+  let remove = document.querySelectorAll(".removeBtn");
+  let countRm = localStorage.getItem("friendItem");
+  let countRmNum = Number(JSON.parse(countRm));
+  for (let i = 0; i < countRmNum; i++) {
+    remove[i].addEventListener("click", removeBtn);
+  }
+}
+function removeBtn() {
+  localStorage.removeItem();
+  showDisplay();
 }
