61228c4c71615a95d4e89974c7b44fba4ed469c7
(youngho) <(one0374@naver.com)>
2020-06-13 23:50:42 +0900

    only need delete

--- todolist-add-friend.js
+++ todolist-add-friend.js
@@ -16,24 +16,32 @@ for (let i = 0; i < friendBtn.length; i++) {
 }
 
 function addFriendBtn(data) {
-  sumTag =
-    `<li><button><i class="fas fa-user-ninja"></i> <span>${friendList[data]}</span><a class = "removeBtn"><h2>X</h2></a></button></li>` +
-    sumTag;
+  sumTag = `<li><button><i class="fas fa-user-ninja"></i> <span>${friendList[data]}</span><a class = "removeBtn"><h2>X</h2></a></button></li>`;
   //내가 버튼을 눌렀을 때 저장은 되지만, 다시 친구 추가 버튼을 누르고 친구들을 골라서 눌렀을 때 다시 초기화가 된다.
   //그래서 friendItem을 자바스크립트 객체로 분해해서 더해준다음 다시 JSON문자열 형식으로 만든 것
   //friendItem보다는 friendItem[0], friendItem[1], friendItem[2] .....friendItem[i]를 배열 형식으로 만드는 게 좋은 것 같다.second
   //그러면 하나하나씩 넣고 뺄 수 있다.
-  let savedItem = localStorage.getItem("friendItem");
-  if (JSON.parse(savedItem) != null) {
-    savedItem = JSON.parse(savedItem) + sumTag;
-    localStorage.setItem("friendItem", JSON.stringify(savedItem));
-    let count = localStorage.getItem("count");
+  // let savedItem = localStorage.getItem("friendItem");
+  // if (JSON.parse(savedItem) != null) {
+  //   savedItem = JSON.parse(savedItem) + sumTag;
+  //   localStorage.setItem("friendItem", JSON.stringify(savedItem));
+  //   let count = localStorage.getItem("count");
+  //   count = Number(JSON.parse(count));
+  //   count += 1;
+  //   localStorage.setItem("count", count);
+  // } else {
+  //   localStorage.setItem("friendItem", JSON.stringify(sumTag));
+  //
+  // }
+  // localStorage.setItem("friendItem", JSON.stringify(sumTag));
+  let count = localStorage.getItem("count");
+  if (JSON.parse(count) != null) {
     count = Number(JSON.parse(count));
+    localStorage.setItem(`friendItem[${count}]`, sumTag);
     count += 1;
     localStorage.setItem("count", count);
   } else {
-    localStorage.setItem("friendItem", JSON.stringify(sumTag));
-    localStorage.setItem("count", 1);
+    localStorage.setItem(`friendItem[0]`, sumTag);
+    localStorage.setItem("count", 0);
   }
-  // localStorage.setItem("friendItem", JSON.stringify(sumTag));
 }
