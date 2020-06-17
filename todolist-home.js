fdbe929205d0210d53821aa9b82f17ab5bf934b4
(youngho) <(one0374@naver.com)>
2020-06-17 11:32:58 +0900

    add comment

--- todolist-home.js
+++ todolist-home.js
@@ -1,9 +1,12 @@
 "use strict";
 firstPlay();
 
+//처음에 localStorage에 있는 데이터들을 Todolist-home에 보이게 할 때 사용하는 함수
 function firstPlay() {
   let originalBtn = document.querySelector("main .main");
   let count = localStorage.getItem("count");
+  //savedLength의 기능은 todolist-add-friend에서 친구를 최대로 추가할 수 있는 횟수다.
+  //최대 추가할 수 있는 횟수가 7회여서 savedLength에는 7이 들어가 있다.
   let savedLength = Number(localStorage.getItem("savedLength"));
   for (let c = 0; c < count.length; c++) {
     for (let i = 0; i < savedLength; i++) {
@@ -24,7 +27,7 @@ function firstPlay() {
     });
   }
 }
-
+//보이지 않은 버튼 데이터를 없애주는 함수
 function remove(data) {
   let num = localStorage.getItem("count");
   let numArray = num.split("");
@@ -33,10 +36,10 @@ function remove(data) {
   numArray.splice(focusNum, 1);
   let numString = numArray.join("");
   localStorage.setItem("count", numString);
-  //초기화
+
   resetPlay(data);
 }
-
+//보이는 버튼을 없애주는 함수
 function resetPlay(data) {
   let buttonReset = document.querySelectorAll(".del");
   buttonReset[data].parentNode.parentNode.style.display = "none";
